55b01a00f13a96e01e962154706c0101808fd532
Patricia Martinez Espert <patricia.martinez@edreamsodigeo.com>
2024-09-24 22:32:05 +0200

    Initial Commit

---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by patricia.martinez on 24/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
